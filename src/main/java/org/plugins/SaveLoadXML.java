0de2fc85fea53097954bda5ad72064acb3f6453c
ValentinoTriadi <valent122016@gmail.com>
2024-05-22 16:36:40 +0700

    feat: load to game state

--- src/main/java/org/plugins/SaveLoadXML.java
+++ src/main/java/org/plugins/SaveLoadXML.java
@@ -1,7 +1,11 @@
 package org.plugins;
 
+import oop.if2210_tb2_sc4.*;
+import oop.if2210_tb2_sc4.Deck;
+import oop.if2210_tb2_sc4.card.*;
 import oop.if2210_tb2_sc4.save_load.Load;
 import oop.if2210_tb2_sc4.save_load.Save;
+import org.jetbrains.annotations.NotNull;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -9,6 +13,7 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.Objects;
 
 
 public class SaveLoadXML implements Save, Load {
@@ -63,6 +68,8 @@ public class SaveLoadXML implements Save, Load {
             }
 
             this.folderName = folderName;
+
+            GameData.initCards();
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
@@ -76,61 +83,127 @@ public class SaveLoadXML implements Save, Load {
 
     public void loadPlayer(int var1) {
         if (var1 == 1 && this.player1 != null) {
-            parsePlayer(this.player1);
+            parsePlayer(this.player1, 1);
             return;
         } else if (var1 == 2 && this.player2 != null) {
-            parsePlayer(this.player2);
+            parsePlayer(this.player2, 2);
             return;
         }
         System.out.println("Failed to load player " + var1);
     }
 
-    private static void parsePlayer(Element playerElement) {
+    private static void parsePlayer(@NotNull Element playerElement, int no_player) {
+        System.out.println();
+        System.out.println("Parsing player");
+        System.out.println();
+
+        Player player = new Player();
+        Deck deck = new Deck();
+        Ladang ladang = new Ladang();
+
+        // Parse gulden and deckCount
+        int Gulden = Integer.parseInt(playerElement.getElementsByTagName("gulden").item(0).getTextContent());
+        System.out.println("Gulden: " + Gulden);
+        player.setJumlahGulden(Gulden);
+        int deckCount = Integer.parseInt(playerElement.getElementsByTagName("deckCount").item(0).getTextContent());
+        System.out.println("Deck count: " + deckCount);
+        deck.setCardsInDeckCount(deckCount);
+
         // Parse hand
         NodeList handList = playerElement.getElementsByTagName("hand");
         if (handList.getLength() > 0) {
             Element handElement = (Element) handList.item(0);
-            System.out.println("Hand count: " + handElement.getAttribute("count"));
+
+            int handCount = Integer.parseInt(handElement.getAttribute("count"));
+            System.out.println("Hand count: " + handCount);
+            deck.setCardsInHandCount(handCount);
+
             NodeList cardList = handElement.getElementsByTagName("card");
             for (int i = 0; i < cardList.getLength(); i++) {
                 Node cardNode = cardList.item(i);
                 if (cardNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element cardElement = (Element) cardNode;
-                    System.out.println("Card lokasi: " + cardElement.getElementsByTagName("lokasi").item(0).getTextContent());
-                    System.out.println("Card name: " + cardElement.getElementsByTagName("name").item(0).getTextContent());
+
+                    String lokasi = cardElement.getElementsByTagName("lokasi").item(0).getTextContent();
+                    System.out.println("Card lokasi: " + lokasi);
+
+                    String nama = cardElement.getElementsByTagName("name").item(0).getTextContent();
+                    System.out.println("Card name: " + nama);
+
+                    deck.setActiveCard(lokasi, GameData.createCard(nama));
                 }
             }
         }
+        player.setDeck(deck);
 
         // Parse ladang
         NodeList ladangList = playerElement.getElementsByTagName("ladang");
         if (ladangList.getLength() > 0) {
             Element ladangElement = (Element) ladangList.item(0);
-            System.out.println("Ladang count: " + ladangElement.getAttribute("count"));
+
+            int ladangCount = Integer.parseInt(ladangElement.getAttribute("count"));
+            System.out.println("Ladang count: " + ladangCount);
+
             NodeList itemList = ladangElement.getElementsByTagName("item");
-            for (int i = 0; i < itemList.getLength(); i++) {
+            for (int i = 0; i < ladangCount; i++) {
                 Node itemNode = itemList.item(i);
                 if (itemNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element itemElement = (Element) itemNode;
-                    System.out.println("Item lokasi: " + itemElement.getElementsByTagName("lokasi").item(0).getTextContent());
-                    System.out.println("Item name: " + itemElement.getElementsByTagName("name").item(0).getTextContent());
-                    System.out.println("Item ageOrWeight: " + itemElement.getElementsByTagName("ageOrWeight").item(0).getTextContent());
 
+                    String name = itemElement.getElementsByTagName("name").item(0).getTextContent();
+                    System.out.println("Item name: " + name);
+
+                    String lokasi = itemElement.getElementsByTagName("lokasi").item(0).getTextContent();
+                    System.out.println("Item lokasi: " + lokasi);
+
+                    int ageOrWeight = Integer.parseInt(itemElement.getElementsByTagName("ageOrWeight").item(0).getTextContent());
+                    System.out.println("Item ageOrWeight: " + ageOrWeight);
+
+                    FarmResourceCard card = (FarmResourceCard) GameData.createCard(name);
+                    assert card != null : "Card not found";
+
+                    // Set age or weight
+                    if (card instanceof PlantCard) {
+                        ((PlantCard) card).setAge(ageOrWeight);
+                    } else if (card instanceof AnimalCard) {
+                        ((AnimalCard) card).setWeight(ageOrWeight);
+                    }
+
+                    // parse Effect
                     NodeList effectList = itemElement.getElementsByTagName("effect");
                     System.out.println("Effects count: " + effectList.getLength());
                     for (int j = 0; j < effectList.getLength(); j++) {
-                        System.out.println("Effect: " + effectList.item(j).getTextContent());
+                        String effect = effectList.item(j).getTextContent();
+                        System.out.println("Effect: " + effect);
+
+                        if (Objects.equals(effect, "ACCELERATE")){
+                            card.addEffect(EffectType.ACCELERATE);
+                        } else if (Objects.equals(effect, "DELAY")){
+                            card.addEffect(EffectType.DELAY);
+                        }  else if (Objects.equals(effect, "INSTANT_HARVEST")){
+                            card.addEffect(EffectType.INSTANT_HARVEST);
+                        } else if (Objects.equals(effect, "PROTECT")){
+                            card.addEffect(EffectType.PROTECT);
+                        } else if (Objects.equals(effect, "TRAP")){
+                            card.addEffect(EffectType.TRAP);
+                        }
                     }
+
+                    ladang.setCard(lokasi, card);
                 }
             }
         }
 
-        // Parse gulden and deckCount
-        System.out.println("Gulden: " + playerElement.getElementsByTagName("gulden").item(0).getTextContent());
-        System.out.println("Deck count: " + playerElement.getElementsByTagName("deckCount").item(0).getTextContent());
+        player.setLadang(ladang);
+        GameState.getInstance().setPlayer(no_player, player);
     }
 
-    private static void parseGameState(Element gameStateElement) {
+    private static void parseGameState(@NotNull Element gameStateElement) {
+        System.out.println();
+        System.out.println("Parsing game state");
+        System.out.println();
+        Shop shop = new Shop();
+
         // Parse items
         NodeList itemsList = gameStateElement.getElementsByTagName("items");
         if (itemsList.getLength() > 0) {
@@ -141,14 +214,21 @@ public class SaveLoadXML implements Save, Load {
                 Node itemNode = itemList.item(i);
                 if (itemNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element itemElement = (Element) itemNode;
-                    System.out.println("Item count: " + itemElement.getElementsByTagName("count").item(0).getTextContent());
-                    System.out.println("Item name: " + itemElement.getElementsByTagName("name").item(0).getTextContent());
+
+                    int count = Integer.parseInt(itemElement.getElementsByTagName("count").item(0).getTextContent());
+                    System.out.println("Item count: " + count);
+                    String name = itemElement.getElementsByTagName("name").item(0).getTextContent();
+                    System.out.println("Item name: " + name);
+
+                    shop.addCard((ProductCard) GameData.getCard(name), count);
                 }
             }
         }
 
         // Parse currentPlayer
-        System.out.println("Current player: " + gameStateElement.getElementsByTagName("currentPlayer").item(0).getTextContent());
+        int currentPlayer = Integer.parseInt(gameStateElement.getElementsByTagName("currentPlayer").item(0).getTextContent());
+        System.out.println("Current player: " + currentPlayer);
+        GameState.getInstance().setCurrentPlayer(currentPlayer);
     }
 
     public void save() {
